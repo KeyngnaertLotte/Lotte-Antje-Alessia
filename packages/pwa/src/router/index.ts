1f8b09150ed1ec30972cd873b7a6624fa6f7fd2e
alessia vinnik <alessia.vinnik@student.howest.be>
2023-09-19 14:46:07 +0200

    2nd issue done

---
+++ packages/pwa/src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import ('../views/Temp.vue')
+    },
+  ]
+})
+
+export default router
