a86380055290e678491275ed83116bb13a25351d
alessia vinnik <alessia.vinnik@student.howest.be>
2023-09-19 15:03:49 +0200

    korte uitleg vue

--- packages/pwa/src/views/NotFound.vue
+++ packages/pwa/src/views/NotFound.vue
@@ -3,6 +3,35 @@
         <h1>404</h1>
         <p>Page not found</p>
         <p>Ooops 😅</p>
+        <p v-if="visible">extra info niet zichtbaar</p>
+        <p v-else>dit zie je wel met v-else</p>
+        <p>{{ visible }}</p>
+
+        <button @click="visible = !visible">Toggle</button>
     </div>
 </template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+    // Vue3 composition API
+    setup(){
+        const visible = ref<boolean>(false)
+        
+        return {
+            visible,
+        }
+    }
+
+    // useEffect(() => {
+    //  //
+    // })
+};
+</script>
+
+
+<style>
+
+</style>
 
