d6d4ed3477c95fcae3d2eb714fe2bd87d691fc27
alessia vinnik <alessia.vinnik@student.howest.be>
2023-09-19 17:47:17 +0200

    lucide installed

--- packages/pwa/src/views/protected/bezoeker/Detail.vue
+++ packages/pwa/src/views/protected/bezoeker/Detail.vue
@@ -1,21 +1,26 @@
 <template>
-    Event: {{ eventId }}
+  <div>Event: {{ eventId }}</div>
+  <Camera />
+  <div>test</div>
 </template>
 
 <script lang="ts">
-import {useRoute} from 'vue-router'
-
+import { useRoute } from 'vue-router'
+import { Camera } from 'lucide-vue-next'
 
 export default {
-    setup() {
-        const { params } = useRoute()
-        
-        console.log('*')
-        console.log(params)
+  components: {
+    Camera,
+  },
+  setup() {
+    const { params } = useRoute()
 
-        return {
-            eventId: params.eventId
-        }
-    },
+    console.log('*')
+    console.log(params)
+
+    return {
+      eventId: params.eventId,
+    }
+  },
 }
 </script>
