1d80635f6d0861345e5cc8a1c6e1255c9598dfb8
EechauteAntje <antje.eechaute@student.howest.be>
2023-09-26 17:16:35 +0200

    database connection docker

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
-import { ArtiestenModule } from './artiesten/artiesten.module';
-import { PersoneelModule } from './personeel/personeel.module';
-import { BezoekersModule } from './bezoekers/bezoekers.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ArtiestenModule } from './artiesten/artiesten.module'
+import { PersoneelModule } from './personeel/personeel.module'
+import { BezoekersModule } from './bezoekers/bezoekers.module'
 
 @Module({
   imports: [
@@ -13,6 +14,16 @@ import { BezoekersModule } from './bezoekers/bezoekers.module';
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27027/api',
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true, // Careful with this in production
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Disable deprecated warnings
+    }),
+    
     ArtiestenModule,
     BezoekersModule,
     PersoneelModule,
