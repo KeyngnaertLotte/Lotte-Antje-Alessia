75f8e2827be5b2700b8514375cd114cb51dd7aaa
alessia vinnik <alessia.vinnik@student.howest.be>
2023-10-04 15:01:17 +0200

    personeel uit database halen

--- packages/api/src/personeel/personeel.resolver.ts
+++ packages/api/src/personeel/personeel.resolver.ts
@@ -3,28 +3,36 @@ import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql'
 import { PersoneelService } from './personeel.service'
 import { Personeel } from './entities/personeel.entity'
 import { CreatePersoneelInput } from './dto/create-personeel.input'
+import { FirebaseGuard } from 'src/authentication/services/guards/firebase.guard'
+import { FirebaseUser } from 'src/authentication/decoraters/user.decorator'
+import { UserRecord } from 'firebase-admin/auth'
+import { UseGuards } from '@nestjs/common'
 
 @Resolver(() => Personeel)
 export class PersoneelResolver {
   constructor(private readonly personeelService: PersoneelService) {}
 
-  @Mutation(() => Personeel, { description: 'Create a bird using the DTO.' })
+  // create personeel
+  @Mutation(() => Personeel)
   createPersoneel(
     @Args('createPersoneelInput') createPersoneelInput: CreatePersoneelInput,
   ): Promise<Personeel> {
     return this.personeelService.create(createPersoneelInput)
   }
 
+  // get all personeel
   @Query(() => [Personeel], { name: 'personeel' })
   findAll() {
     return this.personeelService.findAll()
   }
 
+  // get one personeel
   @Query(() => Personeel, { name: 'personeelLid', nullable: true })
   findOneById(@Args('id') id: string): Promise<Personeel> {
     return this.personeelService.findOneById(id)
   }
 
+  // delete personeel
   @Mutation(() => Personeel)
   removePersoneel(@Args('id', { type: () => Int }) id: number) {
     return this.personeelService.remove(id)
---
+++ packages/pwa/src/views/protected/personeel/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <container>
+    <h1>dashboard Personeel</h1>
+
+    <div v-if="PersoneelLoading">Loading</div>
+    <div v-if="PersoneelError">{{ PersoneelError }}</div>
+    <div v-if="PersoneelData">
+        <div v-for="personeel in PersoneelData.personeel">
+            <p>{{ personeel.voornaam }}</p>
+        </div>
+    </div>
+  </container>
+</template>
+
+<script lang="ts">
+    import { useQuery } from '@vue/apollo-composable';
+    import Container from '@/components/generic/Container.vue';
+    import { ALL_Personeel } from '@/graphql/personeel.query'
+
+    export default {
+        components: { Container},
+
+        setup() {
+            const {loading: PersoneelLoading, error: PersoneelError, result: PersoneelData} = useQuery(ALL_Personeel)
+            return {
+                PersoneelLoading,
+                PersoneelError,
+                PersoneelData,
+            }
+        },
+    }
+
+</script>
