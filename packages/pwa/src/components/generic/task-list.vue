c9a7f729e1e288106b3d53ddf9366aabf4256ca1
alessia vinnik <alessia.vinnik@student.howest.be>
2023-10-10 17:45:30 +0200

    feat: basis takenlijst done

--- packages/pwa/src/components/generic/task-list.vue
+++ packages/pwa/src/components/generic/task-list.vue
@@ -1,15 +1,47 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps } from 'vue'
 
 const props = defineProps({
-    title: {
-        type: String,
-    },
+  title: {
+    type: String,
+  },
 })
 </script>
 
 <template>
-  <div class="bg-pink-300 shadow-lg w-64 h-64 p-4 rounded-md">
+  <div class="bg-white shadow-md w-64 h-64 p-4 rounded-md">
     <h1>Takenlijst</h1>
+    <div class="mt-3">
+      <ul>
+        <li class="flex justify-between">
+          <div>
+            <input type="checkbox" id="checkbox" class="mr-3" />
+            <label for="checkbox">Frisdranken stand A</label>
+          </div>
+          <p>3:32</p>
+        </li>
+        <li class="flex justify-between">
+          <div>
+            <input type="checkbox" id="checkbox" class="mr-3" />
+            <label for="checkbox">Microfoon Antje</label>
+          </div>
+          <p>4:08</p>
+        </li>
+        <li class="flex justify-between">
+          <div>
+            <input type="checkbox" id="checkbox" class="mr-3" />
+            <label for="checkbox">Rietjes stand B</label>
+          </div>
+          <p>3:16</p>
+        </li>
+        <li class="flex justify-between">
+          <div>
+            <input type="checkbox" id="checkbox" class="mr-3" />
+            <label for="checkbox">...</label>
+          </div>
+          <p>3:29</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
