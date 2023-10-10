a9298e1aea99589dba7213d848fbcb03d315426f
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-10-10 12:43:08 +0200

    image added

--- packages/pwa/src/views/Home.vue
+++ packages/pwa/src/views/Home.vue
@@ -1,4 +1,22 @@
 <template>
-    <div>homepagina</div>
-    <div>hier komt de line-up</div>
+    <div class="relative">
+        <div class="absolute w-full grid grid-rows-5">
+            <p class="font-body font-bold text-white text-3xl justify-self-end m-4 row-span-1">LOGIN</p>
+            <div class="flex flex-col items-end row-span-4 justify-center  justify-self-center">
+                <p class="font-header font-bold text-white text-7xl">Lotantsia</p>
+                <p class="font-header font-bold text-white text-5xl">Festival</p>
+            </div>
+        </div>
+        <img src="../images/bg-small.png" alt="" class="w-screen">
+    </div>
+    <div class="">
+        <div>
+            <h1 class="font-header text-green text-4xl">Antje</h1>
+            <div class="w-full">
+                <p class="font-body text-xl">aaaaaaaaaaaaaaa</p>
+                <p class="font-body text-xl">aaaaaaaaaaaaaaa</p>
+            </div>
+            <div></div>
+        </div>
+    </div>
 </template>
