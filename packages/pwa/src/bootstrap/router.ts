e80ca549720d1a1428a39feb72a63151b5d4200a
EechauteAntje <antje.eechaute@student.howest.be>
2023-10-11 15:03:06 +0200

    login roles

--- packages/pwa/src/bootstrap/router.ts
+++ packages/pwa/src/bootstrap/router.ts
@@ -18,17 +18,17 @@ const router = createRouter({
       component: () => import('../views/Festival.vue'),
     },
 
-    {
-      path: '/artiest',
-      component: () =>import('../views/protected/artiest/index.vue'),
-    },
+    // {
+    //   path: '/artiest',
+    //   component: () =>import('../views/protected/artiest/index.vue'),
+    // },
     {
       path: '/bezoeker',
       component: () =>import('../views/protected/bezoeker/Dashboard.vue'),
     },
 
     {
-      path: '/artiest/dashboard',
+      path: '/artiest',
       component: () =>import('../views/protected/artiest/dashboard.vue'),
     },
 
