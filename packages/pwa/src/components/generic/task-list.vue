d834b3dc0bce63dd871f69e8161a83b182ad59e2
alessia vinnik <alessia.vinnik@student.howest.be>
2023-10-11 14:22:01 +0200

    feat: flex replaced with grid

--- packages/pwa/src/components/generic/task-list.vue
+++ packages/pwa/src/components/generic/task-list.vue
@@ -9,39 +9,61 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="bg-white shadow-md w-64 h-64 p-4 rounded-md">
-    <h1>Takenlijst</h1>
+  <div class="bg-white shadow-md max-w-fit p-4 rounded-2xl">
+    <h1 class="font-bold text-xl">Takenlijst</h1>
     <div class="mt-3">
-      <ul>
-        <li class="flex justify-between">
+      <div class="grid grid-cols-2 gap-y-3 grid-rows-2">
+        <div>
+          <input type="checkbox" id="checkbox" class="mr-3" />
+          <label for="checkbox">Frisdranken stand A</label>
+        </div>
+        <p class="text-end">3:32</p>
+        <div>
+          <input type="checkbox" id="checkbox" class="mr-3" />
+          <label for="checkbox">Microfoon Antje</label>
+        </div>
+        <p class="text-end">4:08</p>
+        <div>
+          <input type="checkbox" id="checkbox" class="mr-3" />
+          <label for="checkbox">Rietjes stand B</label>
+        </div>
+        <p class="text-end">3:16</p>
+        <div>
+          <input type="checkbox" id="checkbox" class="mr-3" />
+          <label for="checkbox">...</label>
+        </div>
+        <p class="text-end">3:29</p>
+      </div>
+      <!-- <ul>
+        <li class="flex justify-between my-4">
           <div>
             <input type="checkbox" id="checkbox" class="mr-3" />
             <label for="checkbox">Frisdranken stand A</label>
           </div>
           <p>3:32</p>
         </li>
-        <li class="flex justify-between">
+        <li class="flex justify-between my-4">
           <div>
             <input type="checkbox" id="checkbox" class="mr-3" />
             <label for="checkbox">Microfoon Antje</label>
           </div>
           <p>4:08</p>
         </li>
-        <li class="flex justify-between">
+        <li class="flex justify-between my-4">
           <div>
             <input type="checkbox" id="checkbox" class="mr-3" />
             <label for="checkbox">Rietjes stand B</label>
           </div>
           <p>3:16</p>
         </li>
-        <li class="flex justify-between">
+        <li class="flex justify-between my-4">
           <div>
             <input type="checkbox" id="checkbox" class="mr-3" />
             <label for="checkbox">...</label>
           </div>
           <p>3:29</p>
         </li>
-      </ul>
+      </ul> -->
     </div>
   </div>
 </template>
