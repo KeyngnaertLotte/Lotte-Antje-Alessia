7ca8c361a005d2898d3c324580463cd3e74de44d
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-10-11 15:03:16 +0200

    gitflow-feature-stash: small-fixes

---
+++ packages/pwa/src/components/bezoeker/FavoriteArtists.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="col-span-2 row-start-8 row-span-17 bg-white rounded-xl m-4 grid grid-rows-17">
+        <p class="font-bold font-body text-2xl m-3">FAVORIETE ARTIESTEN</p>
+        <div class="flex flex-row w-full justify-around row-start-3 font-body">
+            <button :class="['border', 'px-6', 'border-3', 'border-custom-darkGreen', 'rounded-lg', {'bg-custom-darkGreen':activeButton === 1}, {'text-white':activeButton === 1}]" @click="handleButtonClick(1)"><p class="font-medium">ANTJE</p></button>
+            <button :class="['border', 'px-6', 'border-3', 'border-custom-purple', 'rounded-lg', {'bg-custom-purple':activeButton === 2}, {'text-white':activeButton === 2}]" @click="handleButtonClick(2)"><p class="font-medium">LOTTE</p></button>
+            <button :class="['border', 'px-6', 'border-3', 'border-custom-orange', 'rounded-lg', {'bg-custom-orange':activeButton === 3}, {'text-white':activeButton === 3}]" @click="handleButtonClick(3)"><p class="font-medium">ALÉSSIA</p></button>
+        </div>
+        <div class="overflow-y-auto row-start-5 row-span-12">
+            <div v-for="(favorite, index) in filteredFavorites" :key="index" :class="['flex', 'flex-col', 'justify-center', 'items-center', 'p-2', 'm-4', 'mt-0', 'rounded-lg', `bg-custom-${favorite.color}`]">
+                <p class="font-bold text-white text-2xl font-body">{{ favorite.artiest }}</p>
+                <p class="text-white text-lg font-body">{{ favorite.time }}</p>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+    import { ref, computed } from 'vue';
+
+    const activeButton = ref(0);
+
+    export default {
+        setup() {
+            const handleButtonClick = (buttonIndex: any) => {
+                console.log(buttonIndex);
+                console.log(activeButton.value);
+                if (activeButton.value === buttonIndex) {
+                    activeButton.value = 0;
+                } else {
+                    activeButton.value = buttonIndex;
+                }
+    };
+
+        const favorites = ref([
+            { artiest: 'BOHNES', time: '20:00 - 21:30', color: 'purple' },
+            { artiest: 'NEFFEX', time: '21:15 - 22:15', color: 'orange' },
+            { artiest: 'BOYWITHUKE', time: '22:30 - 23:30', color: 'orange' },
+            { artiest: 'BERIGHTBACK', time: '23:45 - 01:00', color: 'darkGreen' }
+        ]);
+
+        const filteredFavorites = computed(() => {
+            if (activeButton.value === 1) {
+                return favorites.value.filter((favorite) => favorite.color === 'darkGreen');
+            } else if (activeButton.value === 2) {
+                return favorites.value.filter((favorite) => favorite.color === 'purple');
+            } else if (activeButton.value === 3) {
+                return favorites.value.filter((favorite) => favorite.color === 'orange');
+            } else {
+                return favorites.value;
+            }
+        });
+
+            return { activeButton, handleButtonClick, filteredFavorites };
+        }
+    };
+
+    
+  </script>
--- packages/pwa/src/views/protected/bezoeker/Favorieten.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <div>Mijn favo</div>
-</template>
