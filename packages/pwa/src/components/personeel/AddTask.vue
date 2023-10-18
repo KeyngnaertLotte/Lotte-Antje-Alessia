812d5af4bc7d281015c16ecc41c867846e4ae6c6
alessia vinnik <alessia.vinnik@student.howest.be>
2023-10-18 17:30:43 +0200

    small change

--- packages/pwa/src/components/personeel/AddTask.vue
+++ packages/pwa/src/components/personeel/AddTask.vue
@@ -1,19 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div
-    class="block fixed z-1 left-0 top-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center"
-  >
-    <div
-      class="relative bg-white w-9/10 h-3/4 rounded-lg flex flex-col items-center p-6"
-    >
-      <!-- <button @click="closeModal" class="absolute top-[-1rem] right-[-0.5rem] flex justify-end bg-[#D5573B] rounded-lg"> <X class="z-5 h-12 w-12 fill-white"/></button> -->
-      <button
-        @click="closeModal"
-        class="absolute top-[-1rem] right-[-0.5rem] flex justify-end bg-[#D5573B] rounded-lg h-12 w-12 flex justify-center items-center"
-      >
-        <p class="text-white font-bold font-body text-2xl">X</p>
-      </button>
-    </div>
-  </div>
+
 </template>
