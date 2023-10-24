3ae82e1ea28a1f78a25dcf1ec38121f1328ccdfd
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-10-24 16:34:29 +0200

    einde les

--- packages/api/src/bezoekers/bezoekers.service.spec.ts
+++ packages/api/src/bezoekers/bezoekers.service.spec.ts
@@ -1,18 +1,52 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { BezoekersService } from './bezoekers.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { BezoekersService } from './bezoekers.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Bezoeker } from './entities/bezoeker.entity'
+import { createBezoekerInputStub, bezoekerStub } from './stubs/bezoekers.stub'
 
 describe('BezoekersService', () => {
-  let service: BezoekersService;
+  let service: BezoekersService
+  let mockBezoekerRepository: Repository<Bezoeker>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BezoekersService],
-    }).compile();
+      providers: [
+        BezoekersService,
+        {
+          provide: getRepositoryToken(Bezoeker),
+          useValue: {
+            save: jest.fn().mockResolvedValue(bezoekerStub()),
+          },
+        },
+      ],
+    }).compile()
 
-    service = module.get<BezoekersService>(BezoekersService);
-  });
+    service = module.get<BezoekersService>(BezoekersService)
+    mockBezoekerRepository = module.get<Repository<Bezoeker>>(
+      getRepositoryToken(Bezoeker),
+    )
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+
+  describe('create()', () => {
+    describe('when a bezoeker is created', () => {
+      it('should call bezoekerRepo.save()', () => {
+        const saveSpy = jest.spyOn(mockBezoekerRepository, 'save')
+        const bezoeker = new Bezoeker()
+        service.create(bezoeker)
+        expect(saveSpy).toHaveBeenCalledTimes(1)
+      })
+      it('shoul call bezoekerRepo.save() with the correct params', async() => {
+        const saveSpy = jest.spyOn(mockBezoekerRepository, 'save')
+        const bezoeker = createBezoekerInputStub()
+
+        await service.create(bezoeker)
+        expect(saveSpy).toHaveBeenCalledWith(bezoeker)
+      })
+    })
+  })
+})
