00bab23a8722af7f33f9b898528520b2c078f1d3
alessia vinnik <alessia.vinnik@student.howest.be>
2023-11-02 14:27:11 +0100

    dropdown werkt

--- packages/pwa/src/views/protected/personeel/Materiaal.vue
+++ packages/pwa/src/views/protected/personeel/Materiaal.vue
@@ -3,14 +3,14 @@
     <div v-for="materiaal in materiaalInfo">
       <div v-for="m in materiaal">
         <button
-          @click="showGeluid = !showGeluid"
+          @click="toggleShow(m.categorie)"
           class="m-4 mb-0 p-2 min-w-full border-custom-brown border-1 flex justify-between"
         >
           <p>{{ m.categorie }}</p>
           <ChevronDown class="stroke-custom-brown" />
         </button>
         <transition name="slide-fade">
-          <div v-if="showGeluid">
+          <div v-if="isShow(m.categorie)">
             <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
               <p>{{ m.item }}</p>
               <p>{{ m.aantal }}</p>
@@ -23,26 +23,6 @@
         </transition>
       </div>
     </div>
-
-    <!-- <button
-      @click="showLicht = !showLicht"
-      class="m-4 mb-0 p-2 min-w-full border-custom-brown border-1 flex justify-between"
-    >
-      <p>light</p>
-      <ChevronDown class="stroke-custom-brown" />
-    </button>
-    <transition name="slide-fade">
-      <div v-if="showLicht">
-        <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
-          <p>laser</p>
-          <p>3 pieces</p>
-        </div>
-        <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
-          <p>projectors</p>
-          <p>5 pieces</p>
-        </div>
-      </div>
-    </transition> -->
   </div>
 </template>
 
@@ -52,65 +32,51 @@ import { useQuery } from '@vue/apollo-composable'
 import { ChevronDown } from 'lucide-vue-next'
 import { GET_MATERIAAL } from '@/graphql/materiaal.query'
 
+type ShowState = { [key: string]: boolean }
+
 const materiaalInfo = ref<any | null>(null)
 
 export default {
   name: 'Materiaal',
-  components: {ChevronDown},
+  components: { ChevronDown },
   props: {},
   setup() {
-    //voor elk onderdeel een show variabele
-    const showGeluid = ref(false)
-    const showLicht = ref(false)
-    const showInstrument = ref(false)
-    const showPodium = ref(false)
-    const showBackstage = ref(false)
-    const showStroom = ref(false)
-    const showToilet = ref(false)
-    const showEtenDrinken = ref(false)
-    const showEhbo = ref(false)
-    const showBeveiliging = ref(false)
-    const showAfval = ref(false)
-    const showDecor = ref(false)
+    const showState = ref<ShowState>({})
+
+    const toggleShow = (category: string) => {
+      showState.value[category] = !showState.value[category]
+    }
+
+    const isShow = (category: string) => {
+      return showState.value[category] || false
+    }
 
     const getMateriaalInfo = async () => {
-      try{
+      try {
         const { onResult } = useQuery(GET_MATERIAAL)
         onResult(result => {
-          if (result.data){
+          if (result.data) {
             console.log('Data:', result.data)
             materiaalInfo.value = result.data
             console.log('materiaalInfo:', materiaalInfo.value)
           }
         })
-      }
-      catch(error){
+      } catch (error) {
         console.error('Error fetching materiaal info:', error)
       }
     }
 
     getMateriaalInfo()
     return {
-      showGeluid,
-      showLicht,
-      showInstrument,
-      showPodium,
-      showBackstage,
-      showStroom,
-      showToilet,
-      showEtenDrinken,
-      showEhbo,
-      showBeveiliging,
-      showAfval,
-      showDecor,
-
+      toggleShow,
+      isShow,
       getMateriaalInfo,
       materiaalInfo,
     }
   },
 }
-
 </script>
+
 
 <style scoped>
 .slide-fade-enter-active {
