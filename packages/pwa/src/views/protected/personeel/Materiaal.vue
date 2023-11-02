69316097f8b0f132bb95f3613e4311fcb6f83a72
alessia vinnik <alessia.vinnik@student.howest.be>
2023-11-02 13:02:40 +0100

    basic transition done

--- packages/pwa/src/views/protected/personeel/Materiaal.vue
+++ packages/pwa/src/views/protected/personeel/Materiaal.vue
@@ -1,47 +1,49 @@
 <template>
-  <div class="mt-24 flex flex-col">
-    <button @click="showdeco = !showdeco" class="bg-blue-400 text-white">
-      decoration
+  <div class="mt-24">
+    <button @click="showdeco = !showdeco" class="m-4 mb-0 p-2 min-w-full border-custom-brown border-1 flex justify-between">
+      <p>decoration</p>
+      <ChevronDown class="stroke-custom-brown" />
     </button>
     <transition name="slide-fade">
       <div v-if="showdeco">
-        <div class="m-4 p-2">
+        <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
           <p>balloons</p>
           <p>30 pieces</p>
         </div>
+        <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
+          <p>flowers</p>
+          <p>50 pieces</p>
+        </div>
       </div>
     </transition>
-    <button @click="showlight = !showlight" class="bg-blue-400 text-white">
-      decoration
+
+    <button @click="showlight = !showlight" class="m-4 mb-0 p-2 min-w-full border-custom-brown border-1 flex justify-between">
+      <p>light</p>
+      <ChevronDown class="stroke-custom-brown" />
     </button>
     <transition name="slide-fade">
       <div v-if="showlight">
-        <div class="m-4 p-2">
-          <p>balloons</p>
-          <p>30 pieces</p>
+        <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
+          <p>laser</p>
+          <p>3 pieces</p>
+        </div>
+        <div class="m-2 mx-4 mt-0 p-2 flex justify-between min-w-full">
+          <p>projectors</p>
+          <p>5 pieces</p>
         </div>
       </div>
     </transition>
-    <button @click="showfood = !showfood" class="bg-blue-400 text-white">
-      decoration
-    </button>
-    <transition name="slide-fade">
-      <div v-if="showfood">
-        <div class="m-4 p-2">
-          <p>balloons</p>
-          <p>30 pieces</p>
-        </div>
-      </div>
-    </transition>
+    
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { ChevronDown } from 'lucide-vue-next';
 
-const showdeco = ref(true)
-const showlight = ref(true)
-const showfood = ref(true)
+const showdeco = ref(false)
+const showlight = ref(false)
+const showfood = ref(false)
 </script>
 
 <style scoped>
