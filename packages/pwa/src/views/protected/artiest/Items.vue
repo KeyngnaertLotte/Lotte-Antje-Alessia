f1b78d8de5f2de0726a7f728720491f3f42b3385
EechauteAntje <antje.eechaute@student.howest.be>
2023-11-26 11:30:26 +0100

    added mijn items

--- packages/pwa/src/views/protected/artiest/Items.vue
+++ packages/pwa/src/views/protected/artiest/Items.vue
@@ -1,8 +1,34 @@
 <template>
   <div class="mt-8 col-span-2 row-span-20 flex flex-col items-center">
+    <!-- show benonigdheden in the same way the items are shown -->
+    <div
+      v-if="benodigdheden"
+      class="flex flex-col items-center w-full mb-4 mt-2"
+    >
+      <div class="w-[90%] flex-col items-center justify-center">
+        <button
+          @click="toggleShow('benodigdheden')"
+          class="my-4 p-2 w-full rounded border-1 flex justify-between bg-slate-300"
+        >
+          <p>Bestelde items</p>
+          <ChevronDown />
+        </button>
+
+        <transition name="slide-fade" class="border-2 rounded">
+          <div v-if="isShow('benodigdheden')">
+            <div v-for="item in benodigdheden" :key="item.item">
+              <div class="mx-4 mt-0 p-2 flex justify-between">
+                <p>{{ item.item }}</p>
+                <p>{{ item.aantal }}</p>
+              </div>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
+
     <div v-if="MateriaalByCategorie" class="flex flex-col items-center w-full">
       <div
-      
         v-for="categorie in Object.keys(MateriaalByCategorie)"
         :key="categorie"
         class="w-[90%] flex-col items-center justify-center"
@@ -23,7 +49,7 @@
             >
               <div
                 v-if="item.categorie === categorie"
-                class="mx-4 mt-0 p-2 flex justify-between "
+                class="mx-4 mt-0 p-2 flex justify-between"
               >
                 <p>{{ item.item }}</p>
                 <button @click="minusButtonClicked(item)">
@@ -53,6 +79,7 @@ import { ref, computed } from 'vue'
 import { useQuery } from '@vue/apollo-composable'
 import { ChevronDown, MinusIcon, PlusIcon } from 'lucide-vue-next'
 import { GET_MATERIAAL } from '@/graphql/materiaal.query'
+import { GET_Artiest_By_Uid } from '@/graphql/artiest.query'
 import { useMutation } from '@vue/apollo-composable'
 import { CREATE_ITEM } from '@/graphql/artiest.mutation'
 import useCustomUser from '@/composables/useCustomUser'
@@ -66,6 +93,7 @@ const { mutate: createItem } = useMutation(CREATE_ITEM)
 const materiaalInfo = ref<any | null>(null)
 // const aantal[item] = ref<number>(0)
 const aantal = ref<Record<string, number>>({})
+const benodigdheden = ref<any | null>(null)
 
 interface ItemType {
   item: string
@@ -77,7 +105,7 @@ export default {
   components: { ChevronDown, MinusIcon, PlusIcon },
   setup() {
     const showState = ref<ShowState>({})
-    const { onResult } = useQuery(GET_MATERIAAL)
+    const { onResult, refetch } = useQuery(GET_MATERIAAL)
 
     const toggleShow = (category: string) => {
       showState.value[category] = !showState.value[category]
@@ -101,26 +129,6 @@ export default {
       console.log('submit')
       console.log(aantal)
       console.log(aantal.value)
-      // for (const item in aantal.value) {
-      //   console.log(item)
-      //   console.log(aantal.value[item])
-      //   createItem({
-      //     createBenodigdhedenInput: {
-      //       item: item,
-      //       aantal: aantal.value[item],
-      //       categorie: ,
-      //       deadline: newItemDeadline.value,
-      //     },
-      //     uid: uid,
-      //   })
-      //     .then(graphqlresult => {
-      //       console.log('🎉 new item created in our database')
-      //       console.log(graphqlresult)
-      //     })
-      //     .catch(error => {
-      //       console.error('Error creating item:', error)
-      //     })
-      // }
 
       for (const item in aantal.value) {
         console.log(item)
@@ -145,6 +153,9 @@ export default {
             .then(graphqlresult => {
               console.log('🎉 new item created in our database')
               console.log(graphqlresult)
+
+              // reset aantal
+              aantal.value[item] = 0
             })
             .catch(error => {
               console.error('Error creating item:', error)
@@ -176,6 +187,23 @@ export default {
       return null
     })
 
+    const getBenodigdheden = async () => {
+      console.log('uid:', uid)
+      try {
+        const { onResult } = useQuery(GET_Artiest_By_Uid, { uid })
+        onResult(result => {
+          if (result.data) {
+            console.log('Data:', result.data.artiestByUid.benodigdheden)
+            benodigdheden.value = result.data.artiestByUid.benodigdheden
+          }
+        })
+      } catch (error) {
+        console.error('Error fetching bezoeker info:', error)
+      }
+    }
+
+    getBenodigdheden()
+
     return {
       toggleShow,
       isShow,
@@ -185,6 +213,7 @@ export default {
       minusButtonClicked,
       plusButtonClicked,
       submit,
+      benodigdheden,
     }
   },
 }
