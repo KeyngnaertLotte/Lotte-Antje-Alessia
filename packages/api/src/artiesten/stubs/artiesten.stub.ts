225f26f072d3bd657e46ec60f4df85d43db42a8e
EechauteAntje <antje.eechaute@student.howest.be>
2023-11-28 13:45:12 +0100

    unit test for artiesten

--- packages/api/src/artiesten/stubs/artiesten.stub.ts
+++ packages/api/src/artiesten/stubs/artiesten.stub.ts
@@ -1,9 +1,9 @@
 import { CreateArtiestenInput } from '../dto/create-artiesten.input'
+import { CreateBenodigdhedenInput } from '../dto/create-benodigdheden.input'
 import { Artiest } from '../entities/artiesten.entity'
 export const createartiestInputStub = (): CreateArtiestenInput => {
   const a = new CreateArtiestenInput()
   a.naam = 'Be Right Back'
-  // a.podium = 'Antje'
   a.uid = 'Kz5oJmB8iMMZOKKy8BT9O1BaYZx2'
   return a
 }
@@ -11,7 +11,27 @@ export const createartiestInputStub = (): CreateArtiestenInput => {
 export const artiestStub = (): Artiest => {
   const a = new Artiest()
   a.naam = 'Be Right Back'
-  a.podium = 'Antje'
   a.uid = 'Kz5oJmB8iMMZOKKy8BT9O1BaYZx2'
+  return a
+}
+
+export const artiestStub2 = (): Artiest => {
+  const a = new Artiest()
+  a.naam = 'Antje'
+  a.uid = 'Kz5oJmB8iMMZOKKy8BT9O1BaYZx2'
+  return a
+}
+
+export const benodigdhedenStub = (): Artiest => {
+  const a = new Artiest()
+  a.benodigdheden = [
+    {
+      item: 'cola',
+      podium: 'Lotte',
+      aantal: 1,
+      categorie: 'drank',
+      deadline: '2021-05-05T00:00:00.000Z',
+    },
+  ]
   return a
 }
