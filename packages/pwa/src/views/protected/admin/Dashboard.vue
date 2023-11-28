70c4713c4c5746466a3012b446cd77b81f710c92
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-11-28 15:39:14 +0100

    message en taken eh

--- packages/pwa/src/views/protected/admin/Dashboard.vue
+++ packages/pwa/src/views/protected/admin/Dashboard.vue
@@ -1,9 +1,11 @@
 <template>
   <!-- <Container/> -->
   <div
-    class="row-start-4 col-span-2 row-span-21 grid grid-rows-5 grid-cols-5 gap-4 m-4"
+    class="row-start-4 col-span-2 row-span-21 grid grid-rows-6 grid-cols-7 gap-4 m-4"
   >
-    <div class="bg-white w-full mx-auto p-6 rounded-lg shadow-md row-span-5 col-span-3 ">
+    <div
+      class="bg-white w-full mx-auto p-6 rounded-lg shadow-md row-span-6 col-span-5"
+    >
       <h1 class="text-2xl font-bold mb-4 font-body">Lijst van personeel</h1>
       <div class="flex items-center justify-between mb-4 px-2">
         <h2 class="text-lg font-bold w-1/4">Achternaam</h2>
@@ -14,16 +16,16 @@
             {{ option }}
           </option>
         </select>
-        <p class="w-1/14 flex justify-end items-center text-lg font-bold"> </p>
+        <p class="w-1/14 flex justify-end items-center text-lg font-bold"></p>
       </div>
       <div class="overflow-auto max-h-[80%]">
         <div
-        v-for="(item, index) in personeelInfo"
-        :key="item.id"
-        class="flex items-center justify-between border-b-2 p-2 last:border-b-none"
-      >
-        <p class="w-1/4"> {{ item.achternaam }}</p>
-        <p class="w-1/4">{{ item.voornaam }}</p>
+          v-for="(item, index) in personeelInfo"
+          :key="item.id"
+          class="flex items-center justify-between border-b-2 p-2 last:border-b-none"
+        >
+          <p class="w-1/4">{{ item.achternaam }}</p>
+          <p class="w-1/4">{{ item.voornaam }}</p>
           <select
             class="w-1/4 border p-2 rounded-md"
             v-model="item.type"
@@ -35,16 +37,47 @@
               {{ option }}
             </option>
           </select>
-        <button class="w-1/14 flex justify-center items-center"><Trash2 class=" stroke-1.5"/></button>
+          <button class="w-1/14 flex justify-center items-center">
+            <Trash2 class="stroke-1.5" />
+          </button>
+        </div>
       </div>
-      </div>
-      
     </div>
-    <div class="row-span-2 col-span-2 bg-[#D5573B] rounded-lg shadow-md">
-      <h1>Bericht</h1>
+    <div
+      class="row-span-3 col-span-2 p-6 bg-white rounded-lg shadow-md"
+    >
+      <h1 class="text-2xl font-bold font-body ">Bericht</h1>
+      <form action="" class="flex flex-col justify-between">
+        <div class="flex flex-row w-full my-4">
+          <label for="" class="mr-4 ">Voor: </label>
+          <select name="" id="" class="bg-gray-200 rounded font-pop text-xl focus:outline-none w-3/4 p-1"></select>
+        </div>
+        <textarea
+          placeholder="Typ hier je mededeling"
+          name=""
+          id=""
+          cols="50"
+          rows="3"
+          class="w-full block font-pop border-2 rounded-md  p-1 focus:outline-none focus:border-4 focus:border-white bg-white"
+        ></textarea>
+
+        <button class="py-1 bg-custom-purple text-white my-6 rounded w-1/2 self-end">VERZEND</button>
+      </form>
     </div>
-    <div class="row-span-3 col-span-2 bg-[#D5573B] rounded-lg shadow-md">
-      <h1>Taken toevoegen</h1>
+    <div class="row-span-3 col-span-2 bg-white rounded-lg shadow-md p-6  ">
+      <h1 class="text-2xl font-bold font-body">Voeg taak toe</h1>
+      <form action="" class="flex flex-col justify-around w-full h-full">
+        <div class="flex flex-row w-full my-4">
+          <label for="" class="mr-4 ">Voor: </label>
+          <select name="" id="" class="bg-gray-200 rounded font-pop text-xl focus:outline-none w-3/4 p-1"></select>
+        </div>
+        <div class="flex flex-row">
+          <label for="" class="mr-4">Taak: </label>
+          <input type="text" class="border-b-2 border-b-black w-3/4">
+        </div>
+
+        <button class="py-1 bg-custom-orange text-white my-6 rounded w-1/2 self-end">VERZEND</button>
+      </form>
     </div>
   </div>
 </template>
@@ -56,7 +89,7 @@ import { GET_PERSONEEL } from '@/graphql/personeel.query'
 import { UPDATE_TYPE } from '@/graphql/personeel.mutation'
 import { useMutation } from '@vue/apollo-composable'
 import { ref } from 'vue'
-import { Trash2 } from 'lucide-vue-next';
+import { Trash2 } from 'lucide-vue-next'
 
 interface Personeel {
   id: string
@@ -99,15 +132,15 @@ export default {
         const { onResult } = useQuery(GET_PERSONEEL)
         onResult(result => {
           if (result.data) {
-            const sortedPersoneel = [...result.data.personeel];
+            const sortedPersoneel = [...result.data.personeel]
 
-          // Sort the array by achternaam
-          sortedPersoneel.sort((a: Personeel, b: Personeel) =>
-            a.achternaam.localeCompare(b.achternaam)
-          );
+            // Sort the array by achternaam
+            sortedPersoneel.sort((a: Personeel, b: Personeel) =>
+              a.achternaam.localeCompare(b.achternaam),
+            )
 
-          // Update personeelInfo with the sorted array
-          personeelInfo.value = sortedPersoneel;
+            // Update personeelInfo with the sorted array
+            personeelInfo.value = sortedPersoneel
           }
         })
       } catch (error) {
