255b722b9579de557e8f48a505a074f82d77d98f
alessia vinnik <alessia.vinnik@student.howest.be>
2023-12-11 13:12:09 +0100

    voorlopig commit

--- packages/pwa/src/components/AppBurgerMenu.vue
+++ packages/pwa/src/components/AppBurgerMenu.vue
@@ -3,15 +3,46 @@
     class="max-w-screen max-h-screen h-screen bg-[#D5573B] relative z-1 flex flex-col overflow-hidden md:max-w-1/2 md:absolute md:right-0"
     v-if="isVisible"
   >
-    <div class="flex justify-end p-4">
+    <div class="flex justify-between p-4">
+      <!-- TEST -->
+      <div class="h-[80px]">
+        <button
+          @click="toggleShow(languages)"
+          class="m-4 mb-0 p-2 min-w-full border-white border-1 flex justify-between"
+        >
+          <p class="text-white">
+            <!-- {{ $t('navigation.language', { user: firebaseUser?.displayName }) }} -->
+            {{ $t('navigation.current.language', { user: firebaseUser?.displayName }) }}
+          </p>
+          <ChevronDown class="stroke-white" />
+        </button>
+        <transition name="slide-fade">
+          <div v-if="isShow(languages)">
+            <div>
+              <div
+                v-for="lang in languages"
+                class="m-2 mx-4 mt-0 p-2 flex flex-col justify-between min-w-full"
+              >
+                <p class="text-white">{{ lang }}</p>
+              </div>
+            </div>
+          </div>
+        </transition>
+      </div>
+      <!-- TEST -->
+
       <button @click="toggleVisibility" class="justify-self-end">
         <X class="h-14 w-14 stroke-white" />
       </button>
     </div>
     <div class="flex justify-center items-center py-12">
       <div class="flex flex-col justify-center items-center w-fit">
-        <p class="text-white font-bold text-6xl font-header md:text-4xl">Lotantsia</p>
-        <p class="text-white font-bold text-4xl self-end font-header md:text-3xl">
+        <p class="text-white font-bold text-6xl font-header md:text-4xl">
+          Lotantsia
+        </p>
+        <p
+          class="text-white font-bold text-4xl self-end font-header md:text-3xl"
+        >
           Festival
         </p>
       </div>
@@ -19,20 +50,21 @@
     <div class="px-6 py-12">
       <div class="flex flex-col justify-center gap-8">
         <router-link
-          v-for="(menuItem, index) in menuList" :key="index"
+          v-for="(menuItem, index) in menuList"
+          :key="index"
           :to="menuItem.path"
           @click="toggleVisibility"
           class="flex flex-row items-center text-3xl text-white gap-4 font-body md:text-2xl"
-         >
-          <component :is="menuItem.icon"  class="stroke-white h-6 w-6" /> {{ menuItem.name }}
+        >
+          <component :is="menuItem.icon" class="stroke-white h-6 w-6" />
+          {{ menuItem.name }}
         </router-link>
       </div>
-      
     </div>
     <div class="h-full flex justify-center items-end pb-12">
       <button
         @click="logoutUser"
-        class="flex flex-row items-center text-3xl text-white gap-4 font-body  md:text-2xl"
+        class="flex flex-row items-center text-3xl text-white gap-4 font-body md:text-2xl"
       >
         <LogOut class="stroke-white h-8 w-8 transform -scale-x-100" />
         {{ $t('account.log.out', { user: firebaseUser?.displayName }) }}
@@ -42,28 +74,52 @@
 </template>
 
 <script setup lang="ts">
-import { LayoutDashboard , X, Clock3, Coins, LogOut, UserPlus, PackageOpen, Mic2, ListTodo, ClipboardCheck  } from 'lucide-vue-next'
+import {
+  LayoutDashboard,
+  X,
+  Clock3,
+  Coins,
+  LogOut,
+  UserPlus,
+  PackageOpen,
+  Mic2,
+  ListTodo,
+  ClipboardCheck,
+  ChevronDown,
+} from 'lucide-vue-next'
 import useFirebase from '@/composables/useFirebase'
 import { useRouter } from 'vue-router'
-import { Plus } from 'lucide-vue-next';
+import { Plus } from 'lucide-vue-next'
+import { ref } from 'vue'
 
 const { firebaseUser } = useFirebase()
 
-const props = defineProps(
-  {
-    isVisible: {
-      type: Boolean,
-      required: true,
-    },
-    roleShow: {
-      type: String,
-      required: true,
-    },
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    required: true,
   },
-)
+  roleShow: {
+    type: String,
+    required: true,
+  },
+})
 const emit = defineEmits()
 const { logout } = useFirebase()
 const { replace } = useRouter()
+
+// taal dropdown
+const languages = ['Nederlands', 'Engels']
+type ShowState = { [key: string]: boolean }
+const showState = ref<ShowState>({})
+
+const toggleShow = (languages: any) => {
+  showState.value[languages] = !showState.value[languages]
+}
+
+const isShow = (languages: any) => {
+  return showState.value[languages] || false
+}
 
 const roleMenuList = [
   {
@@ -98,7 +154,7 @@ const roleMenuList = [
         name: 'Items',
         path: '/artiest/items',
         icon: Plus,
-      }
+      },
     ],
   },
   {
@@ -135,7 +191,7 @@ const roleMenuList = [
         icon: UserPlus,
       },
       {
-        name:'Lijst artiesten',
+        name: 'Lijst artiesten',
         path: '/admin/lijst-accounts',
         icon: Mic2,
       },
@@ -143,18 +199,16 @@ const roleMenuList = [
         name: 'Lijst taken',
         path: '/admin/lijst-taken',
         icon: ListTodo,
-      }
+      },
     ],
-  }
+  },
 ]
 
-const menuList = roleMenuList.find((roleMenu) => roleMenu.role === props.roleShow)?.menuList
+const menuList = roleMenuList.find(roleMenu => roleMenu.role === props.roleShow)
+  ?.menuList
 // console.log('menuList:', menuList)
 
-menuList?.forEach(element => {
-  
-  
-});
+menuList?.forEach(element => {})
 
 function toggleVisibility() {
   emit('toggle-visibility') // Emit event to toggle visibility
@@ -166,3 +220,19 @@ const logoutUser = () => {
   })
 }
 </script>
+
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+</style>
