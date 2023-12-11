73e85c79efd94a72e62e3d600dc7a876cdc9fd21
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-12-12 00:00:14 +0100

    Generic - Menu

--- packages/pwa/src/components/AppHeader.vue
+++ packages/pwa/src/components/AppHeader.vue
@@ -5,7 +5,7 @@
     @toggleVisibility="toggleVisibility"
     :roleShow="role"
   />
-  <div class="absolute top-0 left-0 right-0 px-8 py-6 bg-[#D5573B] md:py-5">
+  <div class="absolute top-0 left-0 right-0 px-8 py-6 bg-custom-orange md:py-5">
     <div
       class="text-3xl text-white flex justify-between content-center font-header md:text-2xl"
     >
@@ -13,7 +13,7 @@
         <p>Hey</p>
         <p class="md:pl-3">{{ naam }}!</p>
       </div>
-      <button @click="toggleVisibility">
+      <button @click="toggleVisibility" class="focus:outline-none focus-visible:border-white focus-visible:ring-2 focus-visible:ring-white">
         <AlignJustify class="h-12 w-12 fill-white md:h-8 md:w-8" />
       </button>
     </div>
