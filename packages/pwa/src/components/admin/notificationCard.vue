b274a3d60286fd0b238c2b9288183788ab9f5558
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-12-11 11:42:13 +0100

    notification card smaller

--- packages/pwa/src/components/admin/notificationCard.vue
+++ packages/pwa/src/components/admin/notificationCard.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="row-span-full col-span-2 p-6 bg-white rounded-lg shadow-md col-start-6">
+    <div class="row-span-3 col-span-2 p-6 bg-white rounded-lg shadow-md col-start-6">
       <h1 class="text-2xl font-bold font-body">Bericht</h1>
-      <div class="flex flex-col justify-between h-full">
-        <p class="my-4">Het bericht is voor:</p>
-        <div class="flex flex-row w-full items-center justify-around">
+      <div class="flex flex-col justify-around h-fit gap-4 mt-2">
+          <div>
+            <p class="">Het bericht is voor:</p>
+          <div class="flex flex-row w-full items-center justify-around">
           <div>
             <input
               type="checkbox"
@@ -56,16 +57,17 @@
             <label for="personel">Personeel</label>
           </div>
         </div>
+          </div>
         <textarea
           placeholder="Typ hier je mededeling"
           v-model="messageContent"
           cols="50"
           rows="3"
-          class="w-full block font-pop border-2 rounded-md p-1 focus:outline-none focus:border-4 focus:border-white bg-white my-6 h-full"
+          class="w-full block font-pop border-2 rounded-md p-1 focus:outline-none focus:border-4 focus:border-white bg-white "
         ></textarea>
         <button
           @click="sendMessage"
-          class="py-1 bg-custom-purple text-white my-6 rounded w-1/2 self-end"
+          class="py-1 bg-custom-purple text-white rounded w-1/3 self-end"
         >
           VERZEND
         </button>
