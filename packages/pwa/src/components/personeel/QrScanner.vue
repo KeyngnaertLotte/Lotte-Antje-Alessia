bd1ded54a576bb6035b13f68c3515f91d8857264
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-12-21 15:16:50 +0100

    Personeel: qr scanner done

--- packages/pwa/src/components/personeel/QrScanner.vue
+++ packages/pwa/src/components/personeel/QrScanner.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { ref } from 'vue'
-import { Euro, X } from 'lucide-vue-next'
+import { X } from 'lucide-vue-next'
 import { QrcodeStream } from 'vue-qrcode-reader'
 
 import { useMutation } from '@vue/apollo-composable'
@@ -16,7 +16,6 @@ const selectedPlace = ref("");
 export default {
   components: {
     X,
-    Euro,
     QrcodeStream,
   },
   onMounted() {
@@ -51,7 +50,8 @@ const onDetect = (result: any) => {
       price,
       closeModal,
       onDetect,
-      selectedPlace
+      selectedPlace,
+      enteredValue
     }
   },
 }
@@ -63,55 +63,25 @@ const scanButtonClick = () => {
   enteredValue = price.value
   // console.log('Entered Value:', enteredValue)
   console.log('Selected Place:', selectedPlace.value)
-
-  if (enteredValue === 0 || enteredValue.toString() === '' ) {
-    alert('Please enter a valid price before scanning.')
-    return
-  }
-  else if (selectedPlace.value === '') {
-    alert('Please select a place before scanning.')
-    return
-  }
-   else {
-    showCamera.value = true
-    // console.log('Entered Value:', enteredValue)
-  }
+  showCamera.value = true
 }
 </script>
 
 <template>
-  <div
-    class="block fixed z-1 left-0 top-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center"
-  >
-    <div
-      class="w-7/8 bg-white rounded-md flex items-center justify-center relative"
-      :class="[{ 'h-2/3': showCamera }, { 'h-1/3': !showCamera }]"
-    >
-      <button
-        @click="closeModal"
-        class="absolute z-10 top-[-1rem] right-[-0.5rem] flex flex-row justify-end bg-custom-orange rounded-lg h-12 w-12 flex justify-center items-center hover:bg-custom-brown focus:outline-none focus-visible:border-custom-orange focus-visible:bg-custom-brown focus-visible:ring-2 focus-visible:ring-custom-orange"
-      >
+  <div class="block fixed z-1 left-0 top-0 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
+    <div class="w-7/8 bg-white rounded-md flex items-center justify-center relative" :class="[{ 'h-2/3': showCamera }, { 'h-1/3 pb-2': !showCamera }]">
+      <button @click="closeModal" class="absolute z-10 top-[-1rem] right-[-0.5rem] flex flex-row justify-end bg-custom-orange rounded-lg h-12 w-12 flex justify-center items-center hover:bg-custom-brown focus:outline-none focus-visible:border-custom-orange focus-visible:bg-custom-brown focus-visible:ring-2 focus-visible:ring-custom-orange">
         <X class="h-10 w-10 stroke-white" />
       </button>
-      <div
-        v-if="!showCamera"
-        class="h-full flex flex-col items-center justify-around"
-      >
-        <h1 class="text-2xl font-body font-bold text-custom-orange my-4">
-          Geef een bedrag in:
-        </h1>
+      <form @submit.prevent="scanButtonClick" v-if="!showCamera" class="h-full flex flex-col items-center justify-around">
+        <h1 class="text-2xl font-body font-bold text-custom-orange my-4">Geef een bedrag in:</h1>
         <div class="flex flex-row w-full items-center justify-center mb-4">
-          <Euro class="h-8 w-8" />
-          <input
-            type="number"
-            v-model.number="price"
-            class="block font-pop w-2/5 border-b-2 text-xl border-custom-darkGreen p-1 text-xl focus:outline-none focus-visible:border-custom-orange focus-visible:ring-2 focus-visible:ring-custom-brown focus-visible:ring-2"
-          />
+          <p class="font-pop text-4xl text-custom-darkGreen">€</p>
+          <input type="number" name="amountInput" required v-model="enteredValue" min="0.01" step="0.01" class="block font-pop mx-2 w-9/10 border-b-2 border-custom-darkGreen p-1 text-xl focus:outline-none focus:rounded-lg focus-visible:border-custom-orange focus-visible:ring-2 focus-visible:ring-custom-brown focus-visible:ring-2" />
         </div>
-
-        
-        <div class="w-3/4">
-          <select v-model="selectedPlace" class="w-full bg-gray-200 rounded font-pop p-2 text-xl focus:outline-none focus-visible:border-custom-orange focus-visible:ring-2 focus-visible:ring-custom-brown focus-visible:ring-2">
+        <div class="w-full flex items-center justify-center">
+          
+          <select v-model="selectedPlace" required class="p-2 w-9/10 border-custom-darkGreen rounded-lg border-2 flex justify-between focus:outline-none focus-visible:border-custom-orange focus-visible:ring-2 focus-visible:ring-custom-brown focus-visible:ring-2">
             <option value="" disabled>Selecteer plaats</option>
             <option value="Bar 1">Bar 1</option>
             <option value="Bar 2">Bar 2</option>
@@ -121,20 +91,12 @@ const scanButtonClick = () => {
             <option value="Eetstand 3">Eetstand 3</option>
           </select>
         </div>
-
-        <button
-          class="mt-6 w-full rounded-md bg-custom-orange py-2 font-body font-bold text-2xl text-white flex items-center justify-center gap-4 hover:bg-custom-brown focus:outline-none focus-visible:border-custom-orange focus-visible:bg-custom-brown focus-visible:ring-2 focus-visible:ring-custom-orange"
-          @click="scanButtonClick"
-        >
+        <button type="submit" class="mt-6 w-full rounded-md bg-custom-orange py-2 font-body font-bold text-2xl text-white flex items-center justify-center gap-4 hover:bg-custom-brown focus:outline-none focus-visible:border-custom-orange focus-visible:bg-custom-brown focus-visible:ring-2 focus-visible:ring-custom-orange" >
           Scan
         </button>
-      </div>
+      </form>
       <div v-if="showCamera" class="h-full w-full">
-        <QrcodeStream
-          @detect="onDetect"
-
-          class="h-full w-full"
-        />
+        <QrcodeStream @detect="onDetect" class="h-full w-full" />
       </div>
     </div>
   </div>
