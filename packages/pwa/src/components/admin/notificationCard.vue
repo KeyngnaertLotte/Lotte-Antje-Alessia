15fa305888e509d84186b6a11824fe99b9802518
KeyngnaertLotte <lotte.keyngnaert@student.howest.be>
2023-12-21 22:07:49 +0100

    alert if message empty

--- packages/pwa/src/components/admin/notificationCard.vue
+++ packages/pwa/src/components/admin/notificationCard.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="row-span-full col-span-2 p-6 bg-white rounded-lg shadow-md col-start-6">
-      <h1 class="text-2xl font-bold font-body">Stuur melding</h1>
-      <div class="flex flex-col justify-start h-full gap-4 mt-2">
-          <div class="">
-            <p class="">Het bericht is voor:</p>
-          <div class="flex flex-row w-full items-center justify-around">
+  <div class="row-span-full col-span-2 p-6 bg-white rounded-lg shadow-md col-start-6">
+    <h1 class="text-2xl font-bold font-body">Stuur melding</h1>
+    <div class="flex flex-col justify-start h-full gap-4 mt-2">
+      <div>
+        <p class="">Het bericht is voor:</p>
+        <div class="flex flex-row w-full items-center justify-around">
           <div>
             <input
               type="checkbox"
@@ -57,49 +57,71 @@
             <label for="personel">Personeel</label>
           </div>
         </div>
-          </div>
-        <textarea
-          placeholder="Typ hier je mededeling"
-          v-model="messageContent"
-          cols="50"
-          rows="3"
-          class="w-full block font-pop border-2 rounded-md p-1  bg-white h-7/10 focus:outline-none focus-visible:border-custom-orange focus-visible:ring-2 focus-visible:ring-custom-brown focus-visible:ring-2"
-        ></textarea>
-        <button
-          @click="sendMessage"
-          class="py-1 bg-custom-purple text-white rounded w-1/3 self-end hover:bg-[#9BA0C2] focus:outline-none focus-visible:border-custom-orange focus-visible:bg-[#9BA0C2] focus-visible:ring-2 focus-visible:ring-custom-orange"
-        >
-          VERZEND
-        </button>
       </div>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { ref, watch } from 'vue';
-  import useRealtime from '@/composables/useRealtime'
+      <textarea
+        placeholder="Typ hier je mededeling"
+        v-model="messageContent"
+        cols="50"
+        rows="3"
+        class="w-full block font-pop border-2 rounded-md p-1 bg-white h-7/10 focus:outline-none focus-visible:border-custom-orange focus-visible:ring-2 focus-visible:ring-custom-brown focus-visible:ring-2"
+      ></textarea>
 
-    const { emit } = useRealtime()
+      <!-- Message Sent content -->
+      <div v-if="messageSent" class="text-custom-darkGreen font-bold">
+        Bericht verzonden!
+      </div>
+
+      <!-- Original button -->
+      <button
+        v-if="!messageSent"
+        @click="sendMessage"
+        class="py-1 bg-custom-purple text-white rounded w-1/3 self-end hover:bg-[#9BA0C2] focus:outline-none focus-visible:border-custom-orange focus-visible:bg-[#9BA0C2] focus-visible:ring-2 focus-visible:ring-custom-orange"
+      >
+        VERZEND
+      </button>
+    </div>
+  </div>
+</template>
   
-  export default {
-    setup() {
-      const allChecked = ref(false);
-      const visitorsChecked = ref(false);
-      const artistsChecked = ref(false);
-      const personeelChecked = ref(false);
-  
-      const isAllDisabled = ref(false);
-      const messageContent = ref('');
-  
-      const disableOthers = () => {
-        console.log(allChecked.value);
-        if (!allChecked.value) {
-          isAllDisabled.value =
-            visitorsChecked.value || artistsChecked.value || personeelChecked.value;
-        } 
-      };
+<script lang="ts">
+import { ref, watch, watchEffect } from 'vue';
+import useRealtime from '@/composables/useRealtime';
+
+const { emit } = useRealtime();
+
+export default {
+  setup() {
+    const allChecked = ref(false);
+    const visitorsChecked = ref(false);
+    const artistsChecked = ref(false);
+    const personeelChecked = ref(false);
+
+    const isAllDisabled = ref(false);
+    const messageContent = ref('');
+    const messageSent = ref(false);
+
+    const disableOthers = () => {
+      if (!allChecked.value) {
+        isAllDisabled.value =
+          visitorsChecked.value || artistsChecked.value || personeelChecked.value;
+      }
+    };
   
       const sendMessage = () => {
+        if (messageContent.value === '') {
+          alert('Gelieve een bericht in te vullen');
+        }
+        else if (!allChecked.value && !visitorsChecked.value && !artistsChecked.value && !personeelChecked.value){
+          alert('Gelieve een doelgroep te selecteren');
+        }
+        else{
+        messageSent.value = true;
+
+// Reset the message sent state after 5 seconds
+setTimeout(() => {
+  messageSent.value = false;
+  messageContent.value = ''; // Optionally clear the message input
+}, 5000);
         console.log('send');
         console.log(messageContent.value);
         if (allChecked.value) {
@@ -125,22 +147,39 @@
         }
       };
 
+    }
+
+
+
+
+
+
+
+
+
+      watchEffect(() => {
+      // Reset the message sent state when any checkbox changes
+      messageSent.value = false;
+    });
+
+
       watch([visitorsChecked, artistsChecked, personeelChecked], () => {
         isAllDisabled.value =
           visitorsChecked.value || artistsChecked.value || personeelChecked.value;
       });
   
       return {
-        allChecked,
-        visitorsChecked,
-        artistsChecked,
-        personeelChecked,
-        disableOthers,
-        sendMessage,
-        isAllDisabled,
-        messageContent,
-      };
-    },
-  };
-  </script>
+      allChecked,
+      visitorsChecked,
+      artistsChecked,
+      personeelChecked,
+      disableOthers,
+      sendMessage,
+      isAllDisabled,
+      messageContent,
+      messageSent,
+    };
+  },
+};
+</script>
   
